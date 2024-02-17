fd178e199a48e0a28f27c01914a2660fd1d3f6eb
Aron <emmanuel.aron@gmail.com>
2024-02-17 23:33:42 +0100

    About.jsx: add className for the image thinking man

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
                 Horror Sci-fi, Fantasy and occasionally Roleplaying Games. Shipping from Berlin to the World.
             </p>
             <p>Illustrations by Philipp Simon</p>
-            <img src="src\assets\illustrations\Contact_01.jpg" alt="Contact_01_image" />
+            <img className="thinking" src="src\assets\illustrations\Contact_01.jpg" alt="Contact_01_image" />
         </div>
     );
 }
