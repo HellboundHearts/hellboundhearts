53e337d9a6d7d581cf0ceb6cd23fae845a685e4d
arisar86 <ari.sariannidis@gmail.com>
2024-02-18 16:04:05 +0100

    Form for adding books done

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-function NewArrivals({ books }) {
+const apiURL = "https://books.adaptable.app/books";
+
+function NewArrivals() {
+  const [books, setBooks] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(apiURL)
+      .then((response) => {
+        console.log(response.data);
+        setBooks(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <h1 className="headingArrivals">New Arrivals</h1>
-      <div className="newArrivals">
-        {books &&
-          books.map((book, i) => (
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="newArrivals">
+          {books.map((book, i) => (
             <div key={i}>
               <Link to={`/books/${book.id}`}>
                 <img src={book.image_url} alt={book.title} />
@@ -17,7 +39,8 @@ function NewArrivals({ books }) {
               </Link>
             </div>
           ))}
-      </div>
+        </div>
+      )}
     </>
   );
 }
--- src/pages/AddBooks.jsx
+++ src/pages/AddBooks.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+const apiURL = "https://books.adaptable.app/books";
+
 function AddBooks() {
   //state variables to store values of the inputs
-  const [bookId, setBookId] = useState("");
+  //const [bookId, setBookId] = useState("");
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [year, setYear] = useState("");
@@ -12,9 +14,9 @@ function AddBooks() {
   const [condition, setCondition] = useState("");
   const [price, setPrice] = useState(0);
   const [imageUrl, setImageUrl] = useState("");
-  console.log("test");
+
   //handler functions for the inputs
-  const handleBookId = (e) => setBookId(e.target.value);
+  //const handleBookId = (e) => setBookId(e.target.value);
   const handleTitle = (e) => setTitle(e.target.value);
   const handleAuthor = (e) => setAuthor(e.target.value);
   const handleYear = (e) => setYear(e.target.value);
@@ -41,7 +43,97 @@ function AddBooks() {
     image_url: imageUrl,
   };
 
-  return <div>hello</div>;
+  axios
+    .post(`${apiURL}/new`, newBeer)
+    .then((response) => {
+      console.log(response);
+      navigate("/books");
+    })
+    .catch((error) => {
+      console.log("Error adding new beer:", error);
+    });
+
+  return (
+    <>
+      <div className="d-inline-flex flex-column w-100 p-4">
+        <form>
+          <label>Title</label>
+          <input
+            className="form-control mb-4"
+            type="text"
+            name="title"
+            placeholder="Book Title"
+            value={title}
+            onChange={handleTitle}
+          />
+
+          <label>Author</label>
+          <input
+            className="form-control mb-4"
+            type="text"
+            name="author"
+            placeholder="Books Author"
+            value={author}
+            onChange={handleAuthor}
+          />
+
+          <label>Year</label>
+          <input
+            className="form-control mb-4"
+            type="text"
+            name="year"
+            placeholder="Year"
+            value={year}
+            onChange={handleYear}
+          />
+
+          <label>Genre</label>
+          <input
+            className="form-control mb-4"
+            type="text"
+            name="genre"
+            placeholder="Books Genre"
+            value={genre}
+            onChange={handleGenre}
+          />
+
+          <label>Condition</label>
+          <input
+            className="form-control mb-4"
+            type="text"
+            name="condition"
+            placeholder="Books Condition"
+            value={condition}
+            onChange={handleCondition}
+          />
+
+          <label>Price</label>
+          <input
+            className="form-control mb-4"
+            type="number"
+            name="price"
+            placeholder="Books Price"
+            value={price}
+            onChange={handlePrice}
+            min={0}
+            max={100}
+          />
+
+          <label>Image</label>
+          <input
+            className="form-control mb-4"
+            type="text"
+            name="imageUrl"
+            placeholder="Image URL"
+            value={imageUrl}
+            onChange={handleImageUrl}
+          />
+
+          <button className="btn btn-primary btn-round">Add Book</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default AddBooks;
