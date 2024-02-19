d4c6e7ad41a59bd1222f248601939e25984d1650
Aron <emmanuel.aron@gmail.com>
2024-02-19 10:55:08 +0100

    Shipping

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -4,6 +4,7 @@ function Shipping() {
         <div>
             <div>
                 <p>
+
                     Final prices for shipping will be calculated at checkout depending on weight.
                     Orders will normally be dispatched within 1-2 working days.
                     Standard delivery on your own risk. We do not cover lost parcels.
