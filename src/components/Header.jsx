670fa0d6051a13547730206042b01fc1dba77115
Aron <emmanuel.aron@gmail.com>
2024-02-21 16:50:41 +0100

    Header.jsx header.css shoppingCart.css , responsiveness of the header images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,18 @@ function Header() {
   const [results, setResults] = useState([]);
 
   return (
-    <div>
-      <Link to="/shoppingCart">
-        <FaShoppingCart className="Shopping-Cart" />
-      </Link>
-
-      <Link to="/">
-        <img className="Logo" src="src/assets/illustrations/Logo_01.jpg" />
-      </Link>
-    </div>
+    <div className="header-container">
+  <div className="logo-container">
+    <Link to="/">
+      <img className="Logo" src="src/assets/illustrations/Logo_01.jpg" />
+    </Link>
+  </div>
+  <div className="cart-container">
+    <Link to="/shoppingCart">
+      <FaShoppingCart className="Shopping-Cart" />
+    </Link>
+  </div>
+</div>
   );
 }
 
