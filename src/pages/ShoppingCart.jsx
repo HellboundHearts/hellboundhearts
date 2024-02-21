9aa312a034dfb5cf19ef2b467474964d9cdccb1a
arisar86 <ari.sariannidis@gmail.com>
2024-02-21 18:25:50 +0100

    shoppingcart functionalities finalized

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 function ShoppingCart({ selectedBooks, handleShoppingCartDelete, total }) {
   console.log("Received selectedBooks:", selectedBooks);
 
+  const [selectedOption, setSelectedOption] = useState("standard_europe");
+
+  const options = {
+    standard_europe: { shipping: 8.99, location: 0.0 },
+    tracked_europe: { shipping: 26.99, location: 0.0 },
+    standard_uk: { shipping: 8.99, location: 2.0 },
+    tracked_uk: { shipping: 26.99, location: 2.0 },
+    standard_overseas: { shipping: 18.49, location: 5.0 },
+    tracked_overseas: { shipping: 47.99, location: 5.0 },
+  };
+
   const formattedTotal = typeof total === "number" ? total.toFixed(2) : total;
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const calculateTotalWithShipping = () => {
+    if (selectedBooks.length === 0) {
+      return 0.0;
+    }
+    const selected = options[selectedOption] || {
+      shipping: 0.0,
+      location: 0.0,
+    };
+    return total + selected.shipping + selected.location;
+  };
 
   return (
     <div>
@@ -19,7 +47,23 @@ function ShoppingCart({ selectedBooks, handleShoppingCartDelete, total }) {
           </li>
         ))}
       </ul>
-      <p>Total: ${formattedTotal}</p>
+
+      <div>
+        <label>
+          Shipping Option:
+          <select value={selectedOption} onChange={handleOptionChange}>
+            <option value="standard_europe">Standard Europe - 8.99€</option>
+            <option value="tracked_europe">Tracked Europe - 26.99€</option>
+            <option value="standard_uk">Standard UK - 8.99€ </option>
+            <option value="tracked_uk">Tracked UK - 26,99€</option>
+            <option value="standard_overseas">
+              Standard Overseas - 18,49€
+            </option>
+            <option value="tracked_overseas">Tracked Overseas - 47,99€</option>
+          </select>
+        </label>
+      </div>
+      <p>Total: ${calculateTotalWithShipping().toFixed(2)}</p>
     </div>
   );
 }
