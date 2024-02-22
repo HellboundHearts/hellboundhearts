ba0a51e4dee56d9717d05416cd8c16b2ae54ed34
arisar86 <ari.sariannidis@gmail.com>
2024-02-22 09:52:24 +0100

    changes in css Header/ShoppingCart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/header.css";
 import { FaShoppingCart } from "react-icons/fa";
-import "../css/shoppingCart.css";
-import ShoppingCart from "../pages/ShoppingCart";
 
 function Header() {
   const [results, setResults] = useState([]);
 
   return (
     <div className="header-container">
-  <div className="logo-container">
-    <Link to="/">
-      <img className="Logo" src="src/assets/illustrations/Logo_01.jpg" />
-    </Link>
-  </div>
-  <div className="cart-container">
-    <Link to="/shoppingCart">
-      <FaShoppingCart className="Shopping-Cart" />
-    </Link>
-  </div>
-</div>
+      <div className="logo-container">
+        <Link to="/">
+          <img className="Logo" src="src/assets/illustrations/Logo_01.jpg" />
+        </Link>
+      </div>
+      <div className="cart-container">
+        <Link to="/shoppingCart">
+          <FaShoppingCart className="Shopping-Cart" />
+        </Link>
+      </div>
+    </div>
   );
 }
 
